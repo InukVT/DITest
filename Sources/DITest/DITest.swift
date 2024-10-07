2004129c45ed507946dc1617cf02436ad98ed119
Bastian Inuk Christensen-Knuth <jonalist@gmail.com>
2024-10-07 23:00:31 +0200

    Renamed the generics to be more descriptive

--- Sources/DITest/DITest.swift
+++ Sources/DITest/DITest.swift
@@ -2,30 +2,32 @@ class Services {
     private var services: [String: Any] = [:]
 
     @discardableResult
-    public func add<T>(_ service: T) -> Services {
-        let name = String(describing: T.self)
+    public func add<Service>(_ service: Service) -> Services {
+        let name = String(describing: Service.self)
         services[name] = service
 
         return self
     }
 
     @discardableResult
-    public func add<each D, T>(_ serviceBuilder: (repeat each D) -> T) throws -> Services {
-        let name = String(describing: T.self)
+    public func add<each Dependency, Service>(_ serviceBuilder: (repeat each Dependency) -> Service)
+        throws -> Services
+    {
+        let name = String(describing: Service.self)
         services[name] = try call(callback: serviceBuilder)
 
         return self
     }
 
-    public func get<T>(_ service: T.Type) -> T? {
+    public func get<Service>(_ service: Service.Type) -> Service? {
         let name = String(describing: service)
 
-        let mapped = services[name].flatMap { $0 as? T }
+        let mapped = services[name].flatMap { $0 as? Service }
 
         return mapped
     }
 
-    public func throwingGet<T>(_ service: T.Type) throws(ServiceError) -> T {
+    public func throwingGet<Service>(_ service: Service.Type) throws(ServiceError) -> Service {
         guard let mapped = get(service) else {
             let name = String(describing: service)
             throw ServiceError.noSuchService(name: name)
@@ -34,8 +36,10 @@ class Services {
         return mapped
     }
 
-    public func call<each T, G>(callback: (repeat each T) -> G) throws(ServiceError) -> G {
-        callback(repeat (try throwingGet((each T).self)))
+    public func call<each Dependency, ReturnValue>(
+        callback: (repeat each Dependency) -> ReturnValue
+    ) throws(ServiceError) -> ReturnValue {
+        callback(repeat (try throwingGet((each Dependency).self)))
     }
 }
 
